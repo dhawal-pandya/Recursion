d700b64b72a252b0e0d9421f83f536c23d6a6a6b
Dhawal Pandya <95483771+dhawal-pandya@users.noreply.github.com>
2023-01-24 21:25:43 +0530

    recursive recursion of recursive recursion of recursive recursion...

---
+++ Recursion/1.factorial.js
@@ -0,0 +1,22 @@
+// num = 5
+
+// 5 * 4 * 3 * 2 * 1 = 120;
+
+const num = 5;
+
+const fac = (n) => {
+  // // Iteration
+  //
+  // let factorial = 1;
+  // for (let i = n; i > 0; i--) {
+  //   factorial *= i;
+  // }
+  // return factorial;
+  //
+  // //  Recursion
+  //
+  // if (n === 1) return 1;
+  // return n * fac(n - 1);
+};
+
+console.log(fac(num));
---
+++ Recursion/4.fibonacci.js
@@ -0,0 +1,26 @@
+let num = 5; // goes exponent above 102, goes apeshit above 1476
+
+const fib = (n) => {
+  // // Iteration
+  //
+  // let fibArr = [1, 1];
+  // for (i = 0; i < n - 2; i++) {
+  //   let m = fibArr[i] + fibArr[i + 1];
+  //   fibArr.push(m);
+  // }
+  // //
+  // for (let i = 0; i < fibArr.length; i++) {
+  //   console.log(i + 1, fibArr[i]);
+  // }
+  //
+  // // Recursion
+  //
+  // if (n <= 1) return 1;
+  // return fib(n - 1) + fib(n - 2);
+};
+
+fib(num);
+
+// for (let i = 0; i < num; i++) {
+//   console.log(i + 1, fib(i));
+// } // uncomment for the recursion
